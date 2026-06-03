1b9a45f33b02e8faa51bc06b93010eaef6a8a5cb
Tartorn <tatyana_torn@hotmail.com>
2026-06-03 16:10:07 -0300

    desafio biztrip: created form for test on filament

---
+++ app/Filament/Resources/Tools/Pages/CreateTool.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\Tools\Pages;
+
+use App\Actions\CreateToolAction;
+use App\Filament\Resources\Tools\ToolResource;
+use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
+use Throwable;
+
+class CreateTool extends CreateRecord
+{
+    protected static string $resource = ToolResource::class;
+
+
+    /**
+     * @throws Throwable
+     */
+    public function handleRecordCreation(array $data): Model
+    {
+        $data['user_id'] = auth()->id();
+
+        return app(CreateToolAction::class)->handle($data);
+    }
+}
---
+++ app/Filament/Resources/Tools/Schemas/ToolForm.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Resources\Tools\Schemas;
+
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class ToolForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('title')
+                    ->required(),
+                TextInput::make('link')
+                    ->required(),
+                Textarea::make('description')
+                    ->columnSpanFull(),
+                Select::make('tags')
+                    ->relationship('tags', 'name')
+                    ->multiple()
+                    ->preload(),
+            ]);
+    }
+}
